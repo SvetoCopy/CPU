e334e0e7547392a70a512d689290b18766dee10a
SvetoCopy <65361271+SvetoCopy@users.noreply.github.com>
2024-03-12 00:33:04 +0300

    Update main.cpp

--- SPU/main.cpp
+++ SPU/main.cpp
@@ -6,11 +6,15 @@ int main(int argc, const char* argv[])
 		printf("Usage: %s filename.bin output.txt\n", argv[0]);
 		return -1;
 	}
+	
 	FileInfo file = FileInfoCtor(argv[1]);
 	FILE* out = {};
+	
 	fopen_s(&out, argv[2], "wb");
+	
 	CodeSeg  cs  = {};
 	SPU spu = {};
+	
 	CodeSegCtor(&cs, file.buff_size);
 	SPUCtor(&spu);
 	CodeSegInsertFile(&cs, &file);
